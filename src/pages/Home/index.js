4822d4c9646710e9951886936b4308359fe85088
arfan <arfanabi@icloud.com>
2021-11-24 08:03:03 +0700

    sass loader image loader

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Button from '../../components/elements/Button';
 import * as actions from './action';
 import { initialState, propTypes } from './reducer';
+import styles from './Home.module.scss';
 
 const mapStateToProps = (state) => {
   const { loading } = state.home;
@@ -26,12 +27,13 @@ const Home = (props) => {
     actions.setLoading(true);
     actions.getTest();
   }, []);
-
+console.log('tes')
   return (
-    <>
+    <div className={styles.home}>
       <div>Home</div>
+      <img src="/assets/logo.png" />
       { loading && (<Button />) }
-    </>
+    </div>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');    
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: [
+    './src/webpack-public-path',
+    path.join(__dirname, "src", "index.js")
+  ],
   output: {
     path:path.resolve(__dirname, "dist"),
     publicPath: "/",
   },
   devServer: {
+    contentBase: path.join(__dirname, "public"),
     historyApiFallback: true,
   },
   mode: 'development',
@@ -24,12 +29,61 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]'            
+              },              
+            },
+          },
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: 'assets',
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
     new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
+    }),
+    // Copy assets directory
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" },
+      ],
     }),
   ],
 };
